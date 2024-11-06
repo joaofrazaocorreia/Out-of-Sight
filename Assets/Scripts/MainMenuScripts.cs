ab5c7bb767cd71487b2952c47063398f80c0e6b4
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2024-11-06 14:22:54 +0000

    Add main menu, level select and credits

---
+++ Assets/Scripts/MainMenuScripts.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScripts : MonoBehaviour
+{
+    [SerializeField] [Range(5f, 20f)] private float UIspeed = 10f;
+    [SerializeField] private GameObject background;
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject levelSelectMenu;
+    [SerializeField] private GameObject settingsMenu;
+    [SerializeField] private GameObject creditsMenu;
+    private Dictionary<Transform,Vector3> originalUIPositions;
+
+    private void Start()
+    {
+        originalUIPositions = new Dictionary<Transform, Vector3>();
+
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            originalUIPositions.Add(
+                transform.GetChild(i), transform.GetChild(i).localPosition);
+        }
+    }
+    private IEnumerator MoveUI(Transform uiTransform, Vector3 newPos)
+    {
+        while(uiTransform.localPosition != newPos)
+        {
+            Vector3 difference = newPos - uiTransform.localPosition;
+            float movespeed = Time.deltaTime * UIspeed;
+
+            Vector3 translation = new Vector3(
+                Mathf.Max(difference.x * movespeed, Mathf.Min(movespeed, difference.x)),
+                    Mathf.Max(difference.y * movespeed, Mathf.Min(movespeed, difference.y)),
+                        difference.z);
+
+            uiTransform.localPosition += translation;
+            yield return null;
+        }
+    }
+
+    private void MoveUIToPosition(Transform uiTransform, Vector3 newPos)
+    {
+        StartCoroutine(MoveUI(uiTransform, newPos));
+    }
+
+    private void ReturnUIToOrigin(Transform uiTransform)
+    {
+        if(originalUIPositions.Keys.Contains(uiTransform))
+            StartCoroutine(MoveUI(uiTransform, originalUIPositions[uiTransform]));
+    }
+
+    public void QuitGame()
+    {
+        StopAllCoroutines();
+        Application.Quit();
+    }
+
+    public void OpenLevelSelect()
+    {
+        StopAllCoroutines();
+        MoveUIToPosition(mainMenu.transform, new Vector3(0, 2000, 0));
+        MoveUIToPosition(levelSelectMenu.transform, new Vector3(0, 0, 0));
+    }
+
+    public void OpenSettings()
+    {
+        StopAllCoroutines();
+        MoveUIToPosition(mainMenu.transform, new Vector3(3000, 0, 0));
+        MoveUIToPosition(settingsMenu.transform, new Vector3(0, 0, 0));
+    }
+
+    public void OpenCredits()
+    {
+        StopAllCoroutines();
+        MoveUIToPosition(mainMenu.transform, new Vector3(-3000, 0, 0));
+        MoveUIToPosition(creditsMenu.transform, new Vector3(0, 0, 0));
+    }
+
+    public void BackToMainMenu()
+    {
+        StopAllCoroutines();
+        ReturnUIToOrigin(mainMenu.transform);
+        ReturnUIToOrigin(levelSelectMenu.transform);
+        ReturnUIToOrigin(settingsMenu.transform);
+        ReturnUIToOrigin(creditsMenu.transform);
+    }
+
+    public void LoadScene(int scene)
+    {
+        StopAllCoroutines();
+        SceneManager.LoadScene(scene);
+    }
+}
