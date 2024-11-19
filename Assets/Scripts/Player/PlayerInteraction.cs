71cbd09304d43f102692f470f959a4dd9d452e58
ArKynn <david.f.mendes@pm.me>
2024-11-19 23:17:19 +0000

    Updated test scene, player controller, tags and interaction and input action map

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -1,38 +1,139 @@
+using System;
+using Interaction;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
     [SerializeField] private float raycastDistance;
+    [SerializeField] private float interactionCooldown;
+    [SerializeField] private LayerMask whatIsInteractable;
     [SerializeField] private GameObject raycastOrigin;
     [SerializeField] private GameObject _head;
+    
+    private PlayerInventory _playerInventory;
+    private PlayerEquipment _playerEquipment;
+
+    private float _interactionDuration;
+    private float _interactionCooldownTimer;
+    private bool _finishedInteraction;
+    private bool _interactionReady;
 
     private RaycastHit _hit;
-    private InteractiveObject _activeInteractiveObject;
+
+    private GameObject _activeObject;
+    
+    private InteractiveObject ActiveInteractiveObject
+    {
+        get => _activeInteractiveObject;
+        set
+        {
+            if(value == _activeInteractiveObject) return;
+            
+            _activeInteractiveObject = value;
+            
+            _interactionDuration = _activeInteractiveObject ? _activeInteractiveObject.InteractionDuration : 0;
+        }
+    }
+    
+    [CanBeNull] private InteractiveObject _activeInteractiveObject;
+
+    private void Start()
+    {
+        _playerInventory = GetComponent<PlayerInventory>();
+        _playerEquipment = GetComponent<PlayerEquipment>();
+    }
 
     private void Update()
     {
-        GetInteractiveObject();   
+        GetInteractiveObject();
+        if (!_interactionReady) InteractionCooldown();
     }
 
     private void GetInteractiveObject()
     {
-        Physics.Raycast(raycastOrigin.transform.position, _head.transform.forward, out _hit, raycastDistance);
-        if(_hit.collider != null) _activeInteractiveObject = _hit.collider.gameObject.GetComponent<InteractiveObject>();
+        if (Physics.Raycast(raycastOrigin.transform.position, transform.forward, out _hit, raycastDistance))
+        {
+            var hitInteractable = _hit.collider.GetComponentInParent<InteractiveObject>();
+            ActiveInteractiveObject = hitInteractable;
+        }
+        else
+        {
+            ActiveInteractiveObject = null;
+        }
+            
+    }
+
+    private void InteractionCooldown()
+    {
+        if (_finishedInteraction)
+        {
+            _interactionCooldownTimer = interactionCooldown;
+            _finishedInteraction = false;
+        }
+        
+        _interactionCooldownTimer -= Time.deltaTime;
+        
+        if(_interactionCooldownTimer > 0) return;
+
+        _interactionReady = true;
     }
     
     public void TryInteraction()
     {
-        if(CheckValidInteraction()) Interact();
+        if (!_interactionReady)
+        {
+            _interactionCooldownTimer = interactionCooldown;
+            return;
+        }
+        
+        if(CheckValidInteraction()) Interact(ActiveInteractiveObject.InteractiveType);
     }
     
-
     private bool CheckValidInteraction()
     {
+        return ActiveInteractiveObject != null && CheckCanInteract();
+    }
+
+    private bool CheckCanInteract()
+    {
+        if(!ActiveInteractiveObject.HasRequirement) return true;
+        switch (ActiveInteractiveObject.InteractiveType)
+        {
+            case InteractiveType.DirectItemRequirement:
+                return _playerInventory.HasItem(ActiveInteractiveObject.RequirementObject);
+            
+            case InteractiveType.DirectEquipmentRequirement:
+                return _playerEquipment.CurrentEquipment == ActiveInteractiveObject.RequirementEquipment && _playerEquipment.CurrentEquipment.CanBeUsed;
+        }
         return false;
     }
 
-    private void Interact()
+    private void Interact(InteractiveType interactiveType)
     {
+        _interactionDuration -= Time.deltaTime;
+        if (!(_interactionDuration <= 0f)) return;
         
+        switch (interactiveType)
+        {
+            case InteractiveType.Item:
+                _playerInventory.AddItem(ActiveInteractiveObject.GetComponent<Item>(), 1);
+                break;
+            
+            case InteractiveType.DirectItemRequirement:
+                if(ActiveInteractiveObject.ConsumeRequirement) _playerInventory.RemoveItem(ActiveInteractiveObject.RequirementObject);
+                break;
+            
+            case InteractiveType.DirectEquipmentRequirement:
+                _playerEquipment.CurrentEquipment.Used(ActiveInteractiveObject);
+                break;
+        }
+        
+        ActiveInteractiveObject.Interact();
+        ActiveInteractiveObject = null;
+        
+        _finishedInteraction = true;
+        _interactionReady = false;
     }
 }
