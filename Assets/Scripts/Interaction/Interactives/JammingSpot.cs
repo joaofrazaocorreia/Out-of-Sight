075e280fea5e2cbf80a2ce8cd35ab6c45b761bda
ArKynn <david.f.mendes@pm.me>
2024-11-26 19:36:11 +0000

    Added JammingSpots to cameras, updated JammingSpot, player, scene and tool materials

--- Assets/Scripts/Interaction/Interactives/JammingSpot.cs
+++ Assets/Scripts/Interaction/Interactives/JammingSpot.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 public class JammingSpot : InteractiveObject
 {
     [SerializeField] private GameObject jammable;
+    [SerializeField] private GameObject jammerModel;
     public IJammable Jammable => jammable.GetComponent<IJammable>();
     public override void Interact()
     {
-        if(jammable != null) Jammable.ToggleJammed();
+        if (jammable != null)
+        {
+            Jammable.ToggleJammed();
+            ToggleJammerModel();
+        }
     }
+    
+    private void ToggleJammerModel() => jammerModel.SetActive(!jammerModel.activeSelf);
 }
