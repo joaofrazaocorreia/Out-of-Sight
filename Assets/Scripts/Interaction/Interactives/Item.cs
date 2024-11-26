e36d302ed7a254cd665abe4cf0a2b1376062583e
ArKynn <david.f.mendes@pm.me>
2024-11-26 02:14:56 +0000

    Added item sprites, ui now shows item and equipment sprites

--- Assets/Scripts/Interaction/Interactives/Item.cs
+++ Assets/Scripts/Interaction/Interactives/Item.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 
 public class Item : InteractiveObject
 {
+    [SerializeField] private Sprite icon;
+
     [SerializeField] private string itemName;
     
+    public Sprite Icon => icon;
     public string ItemName => itemName;
     
     public override void Interact()
--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -1,20 +1,32 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
 {
+    [SerializeField] private Sprite emptySlotSprite;
+    private UIManager uIManager;
     private Dictionary<Item, int> inventory = new Dictionary<Item, int>();
+
+    private void Start()
+    {
+        uIManager = FindFirstObjectByType<UIManager>();
+    }
 
     public int GetItemQuantity(Item item) => inventory[item];
     
     public bool HasItem(Item item) => inventory.ContainsKey(item);
     
-    public void AddItem(Item item, int amount) => inventory.Add(item, amount);
-    
+    public void AddItem(Item item, int amount)
+    {
+        inventory.Add(item, amount);
+        uIManager.UpdateInventoryIcon(item.Icon, inventory[item]);
+    }
+
     public void RemoveItem(Item item)
     {
+        uIManager.UpdateInventoryIcon(emptySlotSprite, inventory[item]);
         inventory[item]--;
         if(inventory[item] == 0) inventory.Remove(item);
-        print(inventory);
     }
 }
