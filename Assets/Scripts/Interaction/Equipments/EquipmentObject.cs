81768ebe5f88c37fc3605b84f899ef90f828c382
ArKynn <david.f.mendes@proton.me>
2024-12-10 08:39:30 +0000

    IhasAmmo equipment now correctly update ui when used.

--- Assets/Scripts/Interaction/Equipments/EquipmentObject.cs
+++ Assets/Scripts/Interaction/Equipments/EquipmentObject.cs
@@ -5,15 +5,20 @@ public abstract class EquipmentObject : MonoBehaviour
 {
     [SerializeField] private Sprite icon;
     private GameObject _equipmentModel;
+    private PlayerEquipment _playerEquipment;
     public bool CanBeUsed { get; protected set; }
     public Sprite Icon => icon;
 
-    public abstract void Used(InteractiveObject activeInteractiveObject);
+    public virtual void Used(InteractiveObject activeInteractiveObject)
+    {
+        _playerEquipment.EquipmentUsed();
+    }
     
     public void Equipped(bool isEquipped) => _equipmentModel.SetActive(isEquipped);
 
     protected virtual void Start()
     {
+        _playerEquipment = GetComponentInParent<PlayerEquipment>();
         _equipmentModel = transform.GetChild(0).gameObject;
         Equipped(false);
         CanBeUsed = true;
