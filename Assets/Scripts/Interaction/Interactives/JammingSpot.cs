b3f551bc37943b2794490d0f7c4d0a682b72398b
ArKynn <david.f.mendes@proton.me>
2024-12-18 15:49:07 +0000

    Jammers can now be removed without needing it equipped

--- Assets/Scripts/Interaction/Interactives/JammingSpot.cs
+++ Assets/Scripts/Interaction/Interactives/JammingSpot.cs
@@ -1,19 +1,34 @@
 using Interaction;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class JammingSpot : InteractiveObject
 {
     [SerializeField] private GameObject jammable;
     [SerializeField] private GameObject jammerModel;
+    private Jammer jammer;
     public IJammable Jammable => jammable.GetComponent<IJammable>();
+
+    private void Start()
+    {
+        jammer = FindFirstObjectByType<Jammer>();
+    }
+    
     public override void Interact()
     {
         if (jammable != null)
         {
             Jammable.ToggleJammed();
             ToggleJammerModel();
+            if (!Jammable.Jammed) jammer.CurrentAmmo++;
+            UpdateRequirements(Jammable.Jammed);
         }
     }
     
     private void ToggleJammerModel() => jammerModel.SetActive(!jammerModel.activeSelf);
+
+    private void UpdateRequirements(bool isJammed)
+    {
+        InteractiveType = isJammed ? InteractiveType.DirectNoRequirement : InteractiveType.DirectEquipmentRequirement ;
+    }
 }
