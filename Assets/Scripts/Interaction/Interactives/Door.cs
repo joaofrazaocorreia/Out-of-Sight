3dd113b4ca1834164d71d51d1c0b01c69472b2a9
ArKynn <david.f.mendes@proton.me>
2025-01-08 11:20:42 +0000

    Doors now open when an NPC passes through them

--- Assets/Scripts/Interaction/Interactives/Door.cs
+++ Assets/Scripts/Interaction/Interactives/Door.cs
@@ -1,20 +1,48 @@
+using Unity.AI.Navigation;
 using UnityEngine;
 
 public class Door : InteractiveObject
 {
+    [Header("Door Properties")]
+    [SerializeField] private NavMeshLink navMeshLink;
+    
     private Animator animator;
     private bool _opened;
+    
+    private bool occupied = false;
+
+    public bool Occupied
+    {
+        get => occupied;
+        set
+        {
+            if(occupied == value) return;
+            occupied = value;
+            OpenDoor();
+        }
+    }
     
     private void Awake()
     {
         animator = GetComponentInParent<Animator>();
+    }
+    
+    void Update()
+    {
+        if(navMeshLink == null) return;
+        Occupied = navMeshLink.occupied;
     }
 
     public override void Interact()
     {
         base.Interact();
         
+        OpenDoor();
+    }
+
+    private void OpenDoor()
+    {
         _opened = !_opened;
-        animator.SetTrigger(_opened ? "Open" : "Close");
+        animator.SetTrigger(_opened ? "Open" : "Close");   
     }
 }
