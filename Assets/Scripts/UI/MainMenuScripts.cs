b62ebda407f36e48515c0c319d34571bac5c6926
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2025-02-26 14:18:55 +0000

    Add button highlights to main menu.

--- Assets/Scripts/UI/MainMenuScripts.cs
+++ Assets/Scripts/UI/MainMenuScripts.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -14,14 +15,21 @@ public class MainMenuScripts : MonoBehaviour
     [SerializeField] private GameObject settingsMenu;
     [SerializeField] private GameObject creditsMenu;
     private Dictionary<Transform,Vector3> originalUIPositions;
+    private Dictionary<RectTransform, (Coroutine, float, float, float)> uiButtonScaleUpCoroutines;
+    private Dictionary<RectTransform, (Coroutine, float, float, float)> uiButtonScaleDownCoroutines;
+    private float deltaTime;
 
     private void Start()
     {
         originalUIPositions = new Dictionary<Transform, Vector3>();
+        uiButtonScaleUpCoroutines = new Dictionary<RectTransform, (Coroutine, float, float, float)>();
+        uiButtonScaleDownCoroutines = new Dictionary<RectTransform, (Coroutine, float, float, float)>();
         
         #if UNITY_EDITOR
-            UISpeed *= 8;
+            UISpeed *= 2;
         #endif
+
+        deltaTime = Time.fixedDeltaTime * UISpeed;
 
         for(int i = 0; i < transform.childCount; i++)
         {
@@ -29,16 +37,16 @@ public class MainMenuScripts : MonoBehaviour
                 transform.GetChild(i), transform.GetChild(i).localPosition);
         }
     }
+
     private IEnumerator MoveUI(Transform uiTransform, Vector3 newPos)
     {
         while(uiTransform.localPosition != newPos)
         {
-            Vector3 difference = newPos - uiTransform.localPosition;
-            float movespeed = Time.fixedDeltaTime * UISpeed;
+            Vector3 difference = newPos - uiTransform.localPosition;;
 
             Vector3 translation = new Vector3(
-                Mathf.Max(difference.x * movespeed, Mathf.Min(movespeed, difference.x)),
-                    Mathf.Max(difference.y * movespeed, Mathf.Min(movespeed, difference.y)),
+                Mathf.Max(difference.x * deltaTime, Mathf.Min(deltaTime, difference.x)),
+                    Mathf.Max(difference.y * deltaTime, Mathf.Min(deltaTime, difference.y)),
                         difference.z);
 
             uiTransform.localPosition += translation;
@@ -55,6 +63,77 @@ public class MainMenuScripts : MonoBehaviour
     {
         if(originalUIPositions.Keys.Contains(uiTransform))
             StartCoroutine(MoveUI(uiTransform, originalUIPositions[uiTransform]));
+    }
+
+    private IEnumerator ScaleUI(RectTransform uiTransform, float widthIncrement, float heightIncrement, float textIncrement)
+    {
+        TextMeshProUGUI buttonText = uiTransform.GetComponentInChildren<TextMeshProUGUI>();
+
+        float targetWidth = uiTransform.sizeDelta.x + widthIncrement;
+        float targetHeight = uiTransform.sizeDelta.y + heightIncrement;
+        float targetTextSize = buttonText.fontSize + textIncrement;
+
+        while(uiTransform.sizeDelta.x != targetWidth || uiTransform.sizeDelta.y != targetHeight || buttonText.fontSize != targetTextSize)
+        {
+            float widthDifference = targetWidth - uiTransform.sizeDelta.x;
+            float heightDifference = targetHeight - uiTransform.sizeDelta.y;
+            float textDifference = targetTextSize - buttonText.fontSize;
+
+            if(widthDifference != 0)
+                widthDifference = Mathf.Clamp(widthDifference, -deltaTime*50, deltaTime*50);
+
+            if(heightDifference != 0)
+                heightDifference = Mathf.Clamp(heightDifference, -deltaTime*30, deltaTime*30);
+
+            if(textDifference != 0)
+                textDifference = Mathf.Clamp(textDifference, -deltaTime*30, deltaTime*30);
+
+            uiTransform.sizeDelta += new Vector2(widthDifference, heightDifference);
+            buttonText.fontSize += textDifference;
+            yield return null;
+        }
+    }
+
+    public void HoverScaleUpUI(RectTransform uiTransform)
+    {
+        TextMeshProUGUI buttonText = uiTransform.GetComponentInChildren<TextMeshProUGUI>();
+        if(uiButtonScaleDownCoroutines.Keys.Contains(uiTransform))
+        {
+            StopCoroutine(uiButtonScaleDownCoroutines[uiTransform].Item1);
+
+            uiTransform.sizeDelta = new Vector2(
+                uiButtonScaleDownCoroutines[uiTransform].Item2, uiButtonScaleDownCoroutines[uiTransform].Item3);
+            buttonText.fontSize = uiButtonScaleDownCoroutines[uiTransform].Item4;
+
+            uiButtonScaleDownCoroutines.Remove(uiTransform);
+        }
+
+        if(!uiButtonScaleUpCoroutines.Keys.Contains(uiTransform))
+        {
+            uiButtonScaleUpCoroutines.Add(uiTransform, (StartCoroutine(ScaleUI(uiTransform, 100f, 30f, 20f)),
+                uiTransform.sizeDelta.x + 100f, uiTransform.sizeDelta.y + 30f, buttonText.fontSize + 20f));
+        }
+    }
+
+    public void HoverScaleDownUI(RectTransform uiTransform)
+    {
+        TextMeshProUGUI buttonText = uiTransform.GetComponentInChildren<TextMeshProUGUI>();
+        if(uiButtonScaleUpCoroutines.Keys.Contains(uiTransform))
+        {
+            StopCoroutine(uiButtonScaleUpCoroutines[uiTransform].Item1);
+
+            uiTransform.sizeDelta = new Vector2(
+                uiButtonScaleUpCoroutines[uiTransform].Item2, uiButtonScaleUpCoroutines[uiTransform].Item3);
+            buttonText.fontSize = uiButtonScaleUpCoroutines[uiTransform].Item4;
+
+            uiButtonScaleUpCoroutines.Remove(uiTransform);
+        }
+
+        if(!uiButtonScaleDownCoroutines.Keys.Contains(uiTransform))
+        {
+            uiButtonScaleDownCoroutines.Add(uiTransform, (StartCoroutine(ScaleUI(uiTransform, -100f, -30f, -20f)),
+                uiTransform.sizeDelta.x - 100f, uiTransform.sizeDelta.y - 30f, buttonText.fontSize - 20f));
+        }
     }
 
     public void QuitGame()
@@ -135,5 +214,17 @@ public class MainMenuScripts : MonoBehaviour
             ui.alpha -= Time.fixedDeltaTime;
             yield return null;
         }
+    }
+
+    public void StartFadingInUI(CanvasGroup screen)
+    {
+        screen.alpha = 0f;
+        StartCoroutine(FadeInUI(screen));
+    }
+
+    public void StartFadingOutUI(CanvasGroup screen)
+    {
+        screen.alpha = 1f;
+        StartCoroutine(FadeOutUI(screen));
     }
 }
