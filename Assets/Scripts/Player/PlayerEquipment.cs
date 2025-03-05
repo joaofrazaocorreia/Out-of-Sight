fde95fd00a0babb1901a75152bcca29846534f12
ArKynn <david.f.mendes@proton.me>
2025-03-05 10:59:12 +0000

    Equipment and stamina related UI updates now handled by UIManager class

--- Assets/Scripts/Player/PlayerEquipment.cs
+++ Assets/Scripts/Player/PlayerEquipment.cs
@@ -8,8 +8,8 @@ public class PlayerEquipment : MonoBehaviour
     [SerializeField] private GameObject[] equipments;
     [SerializeField] private PlayAudio equipingPlayer;
 
-    private UIManager uIManager;
     private EquipmentObject[] _equipmentObjects;
+    public EquipmentObject _recentlyAddedEquipment {get; private set;}
     
     public EquipmentObject[] EquipmentObjects => _equipmentObjects;
     
@@ -26,12 +26,15 @@ public class PlayerEquipment : MonoBehaviour
             CurrentEquipment = EquipmentObjects[_currentEquipmentNum];
             CurrentEquipment.Equipped(true);
             EquipmentChanged();
-            uIManager.ToggleAmmoDisplay(CurrentEquipment is IHasAmmo);
-            if(CurrentEquipment is IHasAmmo ammo) uIManager.UpdateAmmoText(ammo.CurrentAmmo + " / " + ammo.MaxAmmo);
+            OnEquipmentChanged?.Invoke(this, EventArgs.Empty);
         }
     }
     
     public EquipmentObject CurrentEquipment { get; private set; }
+
+    
+    public event EventHandler OnEquipmentChanged;
+    public event EventHandler OnEquipmentAdded;
 
     public void NewEquipmentSelected(int index)
     {
@@ -51,11 +54,11 @@ public class PlayerEquipment : MonoBehaviour
 
     private void Unequip()
     {
-        uIManager.ToggleAmmoDisplay(false);
         _currentEquipmentNum = -1;
         CurrentEquipment = null;
 
         GetComponent<Player>().LoseStatus(Player.Status.Suspicious);
+        OnEquipmentChanged?.Invoke(this, EventArgs.Empty);
     }
 
     public void TryUseEquipment()
@@ -70,12 +73,11 @@ public class PlayerEquipment : MonoBehaviour
 
     private void Start()
     {
-        uIManager = FindFirstObjectByType<UIManager>();
         _equipmentObjects = new EquipmentObject[equipments.Length];
         for (int i = 0; i < equipments.Length; i++)
         {
             _equipmentObjects[i] = equipments[i].GetComponent<EquipmentObject>();
-            uIManager.UpdateEquipmentIcon(_equipmentObjects[i].Icon, i);
+            OnEquipmentAdded?.Invoke(this, EventArgs.Empty);
         }
         
         Unequip();
@@ -83,6 +85,6 @@ public class PlayerEquipment : MonoBehaviour
 
     public void EquipmentUsed()
     {
-        if(CurrentEquipment is IHasAmmo ammo) uIManager.UpdateAmmoText(ammo.CurrentAmmo + " / " + ammo.MaxAmmo);
+        OnEquipmentChanged?.Invoke(this, EventArgs.Empty);
     }
 }
