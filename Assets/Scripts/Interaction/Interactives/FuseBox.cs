fdd02f671fb04c8095798bc95610adc4127ce800
ArKynn <david.f.mendes@proton.me>
2025-03-12 09:47:27 +0000

    Added unfinished FuseBox class and prefab, updated Critical Tresspass UI message

---
+++ Assets/Scripts/Interaction/Interactives/FuseBox.cs
@@ -0,0 +1,23 @@
+﻿namespace Interaction
+{
+    public class FuseBox : IJammable
+    {
+        public bool Jammed {get => _jammed;}
+        private bool _jammed;
+        private bool working = true;
+        public void ToggleJammed()
+        {
+            _jammed = !_jammed;
+            if (Jammed && working)
+            {
+                working = false;
+                AlertCameraOperator();
+            }
+        }
+
+        private void AlertCameraOperator()
+        {
+            
+        }
+    }
+}
